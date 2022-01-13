3a9b6f7467e2af851dd761cc22c24e107a3faf3a
Renato D. Schneider <renatodaltiba@gmail.com>
2022-01-13 14:26:53 -0400

    dasd

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import { Header } from "../components/Header";
+import { SessionProvider } from "next-auth/react";
+
+import "../styles/global.scss";
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <>
+      <SessionProvider session={session}>
+        <Header />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>ig.news</title>
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          ></link>
+
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
